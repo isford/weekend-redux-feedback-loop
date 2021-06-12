162676119e1c26cfaaf07d7db249569aa4ded29d
Ian <isford7@gmail.com>
2021-06-12 10:44:45 -0500

    Info now appending to DOM for review

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {useSelector} from 'react-redux';
+
 
 export default function Review() {
+    const surveyList = useSelector(store => store.surveyList);
+
     return (
         <div>
             <p>STUFF TO REVIEW</p>
+            <ul>
+                {surveyList.map((response, index) =>
+                <li key = {index}>{response.feeling}</li>
+                )}
+            </ul>
             <button>SUBMIT IT</button>
         </div>
-    )
+    );
+   
 }
